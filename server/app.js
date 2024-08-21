b98c7f9bbba13e204d337ba63c3ad42b5c506b5a
backer-createcode <wecreatecode@gmail.com>
2024-08-21 16:51:14 +0800

    removed limiter

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ app.use(passport.authenticate("session"));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(limiter);
+// app.use(limiter);
 
 // // MongoDB connection
 mongoose.connect(process.env.MONGO_URI);
